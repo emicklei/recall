1654c2c6af3af95b023549224bec5ce0f430e223
ernest micklei <ernest.micklei@gmail.com>
2025-02-17 10:50:09 +0100

    handle panic to return error with stack

---
+++ examples/recording/panic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/emicklei/recall"
+)
+
+func main() {
+	recaller := recall.New(context.Background()).WithCaptureStrategy(recall.RecordingStrategy)
+
+	err := recaller.Call(func(ctx context.Context) error {
+		rlog := recall.LoggerFromContext(ctx)
+
+		rlog.Debug("this will show up on panic")
+		doPanic()
+		return nil
+	})
+
+	slog.Error("bummer", "err", err)
+}
+
+func doPanic() { panic("boom") }
